c182699f942e42d14c0efbf3e26377780d1e581e
Lionsht777 <117496576+Lionsht777@users.noreply.github.com>
2023-04-03 11:24:37 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
           },
         ],
         permanent: true,
-        destination: 'https://orgvn.top/:slug',
+        destination: 'https://wfpnews.com/:slug',
       },
     ];
   },
